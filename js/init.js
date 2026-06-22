2fced6204eb71e1c88c7698a2dfcfbdb740f9977
Chris Small Test <cjt@turnip.cjs.net>
2026-06-22 13:17:44 +1000

    Replaced init.js with Brendan's new code.

--- js/init.js
+++ js/init.js
@@ -1,13 +1,29 @@
-// File: /js/init.js
 
-import { consoleLogDisabled, consoleLog, preloadAssets }     from '../js/utils.js';
-import { buildDOM }     from '../js/build.js';
+import {
+    consoleLogDisabled,
+    consoleLog,
+    preloadAssets
+} from "../js/utils.js";
+
+import { buildDOM } from "../js/build.js";
+
 export async function initApp() {
-    consoleLogDisabled("Mia: Logging Disabled in ../js/utils.js, only errors and warns will be displayed!");
+    consoleLogDisabled(
+        "Mia: Logging Disabled in ../js/utils.js, only errors and warns will be displayed!"
+    );
+
     consoleLog("[Initialize] [Build DOM]");
     await buildDOM();
+   
+    requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+            void preloadAssets();
+        });
+    });
 }
 
 consoleLog("[Initialize]");
-preloadAssets();
-window.addEventListener('DOMContentLoaded', () => initApp());
+
+window.addEventListener("DOMContentLoaded", () => {
+    void initApp();
+});
